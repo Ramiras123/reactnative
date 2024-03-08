dd9d1dfce6c65a46c2e156e15fe411ced2b3410f
Aleksandr <smirnow201494@gmail.com>
2024-03-08 16:17:11 +0300

    настройка роутинга

---
+++ native-app/app/(app)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const RootIndex = () => {
+	return (
+		<View>
+			<Text>RootIndex</Text>
+		</View>
+	);
+};
+
+export default RootIndex;
+
+const styles = StyleSheet.create({});
