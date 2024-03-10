406e5d5c37f006ba62064a0d73f872d648db9450
Aleksandr <smirnow201494@gmail.com>
2024-03-10 16:39:56 +0300

    redirect auth

--- native-app/app/(app)/_layout.tsx
+++ native-app/app/(app)/_layout.tsx
@@ -1,7 +1,13 @@
-import { SplashScreen, Stack } from 'expo-router';
+import { Redirect, SplashScreen, Stack } from 'expo-router';
+import { useAtomValue } from 'jotai';
+import { authAtom } from '../../entities/auth/model/auth.state';
 
 SplashScreen.preventAutoHideAsync();
 export default function AppLayout() {
+	const { access_token } = useAtomValue(authAtom);
+	if (!access_token) {
+		return <Redirect href={'/login'}></Redirect>;
+	}
 	return (
 		<>
 			<Stack>
--- native-app/app/(app)/index.tsx
+++ native-app/app/(app)/index.tsx
@@ -1,17 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect } from 'react';
-import { useAtomValue, useSetAtom } from 'jotai';
-import { authAtom, logoutAtom } from '../../entities/auth/model/auth.state';
-import { router, useRootNavigationState } from 'expo-router';
+import React from 'react';
 const RootIndex = () => {
-	const { access_token } = useAtomValue(authAtom);
-	const state = useRootNavigationState();
-	useEffect(() => {
-		if (!state?.key) return;
-		if (!access_token) {
-			router.replace('/login');
-		}
-	}, [access_token, state]);
 	return (
 		<View>
 			<Text>index</Text>
