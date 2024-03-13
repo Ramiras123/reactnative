6e8b34eebf10d420967d72eb9dab296fa785c120
Aleksandr <smirnow201494@gmail.com>
2024-03-13 18:41:45 +0300

    получение профиля

--- native-app/entities/user/model/user.state.ts
+++ native-app/entities/user/model/user.state.ts
@@ -1,5 +1,8 @@
 import { atom } from 'jotai';
 import { User } from './user.model';
+import { authAtom } from '../../auth/model/auth.state';
+import axios, { AxiosError } from 'axios';
+import { API } from '../api/api';
 
 export interface UserState { 
 	profile: User | null;
@@ -11,4 +14,38 @@ export const profileAtom = atom<UserState>({
 	profile: null, 
 	isLoading: false, 
 	error: null
+})
+
+
+export const loadProfileAtom = atom(async (get) => {
+	return get(profileAtom)
+}, async(get, set) => {
+	const { access_token} = await get(authAtom)
+	set(profileAtom, {
+		isLoading: true,
+		profile: null,
+		error: null
+	})
+	try	 {
+		const { data } = await axios.get<User>(API.profile, {
+				headers: {
+					
+					Authorization: `Bearer ${access_token}`,
+				},
+			});
+			set(profileAtom, {
+				isLoading: false,
+				profile: data,
+				error: null
+			})
+	} catch (e) {
+		if(e instanceof AxiosError ){ 
+			set(profileAtom, {
+				isLoading: false,
+				profile: null,
+				error: e.response?.data.message
+			})
+		}
+	}
+
 })
