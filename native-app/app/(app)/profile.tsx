3ed2bc3cd90df7f47543fc00ce98ddd562e79333
Aleksandr <smirnow201494@gmail.com>
2024-03-24 21:47:20 +0300

    Добавления кнопки для загрузки изображений

--- native-app/app/(app)/profile.tsx
+++ native-app/app/(app)/profile.tsx
@@ -1,86 +1,44 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import React, { useState } from 'react';
-import {
-	launchCameraAsync,
-	MediaTypeOptions,
-	useCameraPermissions,
-	PermissionStatus,
-	useMediaLibraryPermissions,
-	launchImageLibraryAsync
-} from 'expo-image-picker';
-import Button from '../../shared/Button/Button';
+import ImageUploader from '../../shared/ImageUploader/ImageUploader';
+import { Gaps } from '../../shared/tokens';
 
 const Profile = () => {
 	const [image, setImage] = useState<string | null>(null);
-	const [cameraPermissions, setCameraPermissions] = useCameraPermissions();
-	const [libraryPermissions, setLibraryPermissions] =
-		useMediaLibraryPermissions();
-
-	const verificationCameraPermissions = async () => {
-		if (cameraPermissions?.status === PermissionStatus.UNDETERMINED) {
-			const res = await setCameraPermissions();
-			return res.granted;
-		}
-		if (cameraPermissions?.status === PermissionStatus.DENIED) {
-			return false;
-		}
-		return true;
-	};
-
-	const verificationLibraryPermissions = async () => {
-		if (libraryPermissions?.status === PermissionStatus.UNDETERMINED) {
-			const res = await setLibraryPermissions();
-			return res.granted;
-		}
-		if (libraryPermissions?.status === PermissionStatus.DENIED) {
-			return false;
-		}
-		return true;
-	};
-
-	const captureAvatar = async () => {
-		const isVerified = await verificationCameraPermissions();
-		if (!isVerified) {
-			return;
-		}
-		const result = await launchCameraAsync({
-			mediaTypes: MediaTypeOptions.Images,
-			allowsEditing: true,
-			aspect: [1, 1],
-			quality: 0.5
-		});
-		if (!result.assets) {
-			return;
-		}
-		setImage(result.assets[0].uri);
-	};
-
-	const pickAvatar = async () => {
-		const isVerified = await verificationLibraryPermissions();
-		if (!isVerified) {
-			return;
-		}
-		const result = await launchImageLibraryAsync({
-			mediaTypes: MediaTypeOptions.Images,
-			allowsEditing: true,
-			aspect: [1, 1],
-			quality: 0.5
-		});
-		if (!result.assets) {
-			return;
-		}
-		setImage(result.assets[0].uri);
-	};
 	return (
-		<View>
-			<Text>Profile</Text>
-			<Button text="Сфотографировать" onPress={captureAvatar} />
-			<Button text="Выбрать изображение" onPress={pickAvatar} />
-			{image && <Image source={{ uri: image, width: 100, height: 100 }} />}
+		<View style={styles.container}>
+			{image ? (
+				<Image
+					style={styles.image}
+					source={{ uri: image, width: 100, height: 100 }}
+				/>
+			) : (
+				<Image
+					style={styles.image}
+					source={require('../../assets/avatar-none.png')}
+					resizeMode="contain"
+				/>
+			)}
+
+			<ImageUploader onUpload={setImage} />
 		</View>
 	);
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		flexDirection: 'row',
+		gap: Gaps.g16,
+		paddingHorizontal: 30,
+		paddingVertical: 30,
+		alignSelf: 'center'
+	},
+	image: {
+		width: 70,
+		height: 70,
+		borderRadius: 35
+	}
+});
